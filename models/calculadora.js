f894cd2175fe955fc3cb5527c41489b22b074486
João Paulo Giron <136199516+Giron-jpg@users.noreply.github.com>
2024-09-19 18:54:57 +0000

    adicionando o endpoint `api/v1/status`

--- models/calculadora.js
+++
@@ -1,8 +0,0 @@
-function somar(valor1 = 0, valor2 = 0) {
-  if (typeof valor1 !== "number" || typeof valor2 !== "number") {
-    return "Erro";
-  }
-  return valor1 + valor2;
-}
-
-exports.somar = somar;
